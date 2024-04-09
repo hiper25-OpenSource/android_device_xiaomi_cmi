0f9a63e539637a27e7e4dcae0bbf59c41e80cfe8
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-04-09 16:49:58 +0200

    cmi: udfps: Implement dummy enroll methods
     * would break build with [1] otherwise
    
    [1] https://gerrit.aospa.co/c/AOSPA/android_hardware_xiaomi/+/39554
    
    Change-Id: Ic36bd71a65dd98391f95e2f893405482dacc7278

--- udfps/UdfpsHandler.cpp
+++ udfps/UdfpsHandler.cpp
@@ -116,6 +116,18 @@ class XiaomiKonaUdfpsHandler : public UdfpsHandler {
         int arg[2] = {TOUCH_FOD_ENABLE, FOD_STATUS_OFF};
         ioctl(touch_fd_.get(), TOUCH_IOC_SETMODE, &arg);
     }
+
+    void preEnroll() {
+        LOG(DEBUG) << __func__;
+    }
+
+    void enroll() {
+        LOG(DEBUG) << __func__;
+    }
+
+    void postEnroll() {
+        LOG(DEBUG) << __func__;
+    }
   private:
     fingerprint_device_t *mDevice;
     android::base::unique_fd touch_fd_;
