781347d43901813aacbd43ac2c7a868a2b901450
hiper25 <2243168976@qq.com>
2024-09-18 11:29:48 +0800

    cmi: overlay: Convert to RRO packages
    * CheckOut overlays and remove unused.
    
    * Update Status Bar from Stock.

--- device.mk
+++ device.mk
@@ -32,10 +32,12 @@ PRODUCT_COPY_FILES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
 PRODUCT_PACKAGES += \
+    FrameworksResCmi \
+    SettingsResCmi \
+    SystemUIResCmi \
     TargetWifiOverlay \
     xiaomi_cmi_hwlevel_default_overlay \
     xiaomi_cmi_hwlevel_p01_overlay \
