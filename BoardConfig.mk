6e6c4a2b68e1d3460781c0758a87738205249ca0
hiper25 <whydonit@gmail.com>
2025-02-10 03:18:22 +0800

    cmi: Switch to common xiaomi framework compatibility matrix
    Change-Id: If03d07c79ebddf818f8d18c9cb377eba44328919

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,7 +66,8 @@ TARGET_SURFACEFLINGER_UDFPS_LIB := //hardware/xiaomi:libudfps_extension.xiaomi
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
-    $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml
+    $(DEVICE_PATH)/vintf/xiaomi_framework_matrix.xml \
+    hardware/xiaomi/vintf/xiaomi_framework_compatibility_matrix.xml
 
 DEVICE_MANIFEST_FILE := \
     $(DEVICE_PATH)/vintf/manifest.xml \
