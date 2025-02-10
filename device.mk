eae13394c3b6a25fb2ab684b2a3b9f7e4fde5e01
hiper25 <whydonit@gmail.com>
2025-02-11 03:30:00 +0800

    cmi: Build mlipay from prebuilt libs
    Change-Id: I91bc3520b396d978e9cc61bbaca1ab5aca738d09

--- device.mk
+++ device.mk
@@ -148,10 +148,6 @@ TARGET_HEALTH_CHARGING_CONTROL_SUPPORTS_BYPASS := false
 
 # Mlipay
 PRODUCT_PACKAGES += \
-    vendor.xiaomi.hardware.mlipay@1.1.vendor \
-    vendor.xiaomi.hardware.mtdservice@1.0.vendor
-
-PRODUCT_PACKAGES += \
     IFAAService
 
 # NFC
