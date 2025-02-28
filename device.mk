8f28c1e07b3bbd73fc977bb3296e80f99cd32738
hiper25 <whydonit@gmail.com>
2025-03-01 02:05:59 +0800

    cmi: Remove Light AIDL
    * There are no lights on this unit

--- device.mk
+++ device.mk
@@ -196,10 +196,6 @@ PRODUCT_PACKAGES += \
 # Kernel
 PRODUCT_SET_DEBUGFS_RESTRICTIONS := true
 
-# Lights
-PRODUCT_PACKAGES += \
-    android.hardware.light-service.xiaomi
-
 # Lineage Health
 PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
