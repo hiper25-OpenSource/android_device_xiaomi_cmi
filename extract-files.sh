b00a5d1728b32f7d3fd5c1e67acfdef6e4dcc07d
hiper25 <whydonit@gmail.com>
2025-05-27 22:07:47 +0800

    cmi: Allow gettid syscall for atfwd@2.0 Change-Id: I0d7854b73e0b7e822edce89b9a3d6e33e1f45fc6

--- extract-files.sh
+++ extract-files.sh
@@ -55,13 +55,20 @@ fi
 
 function blob_fixup() {
     case "${1}" in
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            grep -q 'gettid: ' "${2}" || echo 'gettid: 1' >> "${2}"
+            ;;
         vendor/etc/init/init.batterysecret.rc)
+            [ "$2" = "" ] && return 0
             sed -i "/seclabel u:r:batterysecret:s0/d" "${2}"
             ;;
         vendor/lib64/vendor.qti.hardware.camera.postproc@1.0-service-impl.so)
+            [ "$2" = "" ] && return 0
             $SIGSCAN -p "9A 0A 00 94" -P "1F 20 03 D5" -f "${2}"
             ;;
         vendor/lib64/camera/components/com.mi.node.watermark.so )
+            [ "$2" = "" ] && return 0
             grep -q "libpiex_shim.so" "${2}" || $PATCHELF --add-needed "libpiex_shim.so" "${2}"
             ;;
     esac
